9931aa65fd3e0fd156204ecd3b4ffdf710cc134a
masayuki-kono@mamezou.com <masayuki-kono@gmail.com>
2023-09-09 00:50:18 +0900

    add configurations for color and depth images

--- h264_video_encoder/launch/h264_video_encoder.launch.py
+++ h264_video_encoder/launch/h264_video_encoder.launch.py
@@ -10,43 +10,48 @@
 # on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
 # express or implied. See the License for the specific language governing
 # permissions and limitations under the License.
-import os
 
-from ament_index_python.packages import get_package_share_directory
-import launch
-import launch_ros.actions
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.actions import DeclareLaunchArgument
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import OpaqueFunction
+from launch.actions import LogInfo
+from launch_ros.actions import Node
 
-# Argument names
-NODE_NAME = "node_name"
-CONFIG = "config"
 
-def generate_launch_description():
+def launch_setup(context, *args, **kwargs):
+  node_name = LaunchConfiguration("node_name")
+  config_filename = LaunchConfiguration("config_filename")
 
-  # Default to included config file
-  default_config = os.path.join(get_package_share_directory('h264_video_encoder'),
-    'config', 'sample_configuration.yaml')
-
-  ld = launch.LaunchDescription([
-    launch.actions.DeclareLaunchArgument(
-      NODE_NAME,
-      default_value="h264_video_encoder",
-    ),
-    launch.actions.DeclareLaunchArgument(
-      CONFIG,
-      default_value=default_config
-    )
-   ])
-  encoder_node = launch_ros.actions.Node(
-    package="h264_video_encoder",
-    executable="h264_video_encoder",
-    name=launch.substitutions.LaunchConfiguration(NODE_NAME),
-    parameters=[launch.substitutions.LaunchConfiguration(CONFIG)]
+  config_filepath = PathJoinSubstitution(
+      [FindPackageShare("h264_video_encoder"), "config", config_filename]
   )
 
-  ld.add_action(encoder_node)
+  encoder_node = Node(
+    package="h264_video_encoder",
+    executable="h264_video_encoder",
+    name=node_name,
+    parameters=[config_filepath]
+  )
 
-  return ld
+  output_log_actions = [LogInfo(msg=config_filepath)]
+  return output_log_actions + [encoder_node]
 
 
-if __name__ == "__main__":
-  generate_launch_description()
+def generate_launch_description():
+  declared_arguments = []
+  declared_arguments.append(
+      DeclareLaunchArgument(
+          "node_name",
+          default_value="h264_video_encoder",
+      )
+  )
+  declared_arguments.append(
+      DeclareLaunchArgument(
+          "config_filename",
+          default_value="color.yaml",
+      )
+  )
+
+  return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
